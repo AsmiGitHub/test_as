76d55011b105ff08adfd2da3ff920172380b4ff3
AsmiGitHub <asmi.ghimire077@gmail.com>
2023-05-14 13:34:49 +1000

    few changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div>
         
             <div className="HeaderDiv"> 
-            <h3>Welcome to the Comment App</h3>
+            <h3>Welcome to the Comment App Guys</h3>
             </div>
             </div>
 )};
